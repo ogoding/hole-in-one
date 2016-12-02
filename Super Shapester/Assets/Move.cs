69d38564c86816f413e26e0eda2bba18ef8343ea
Matt Noone <matt.noone95@gmail.com>
2016-12-03 09:34:21 +1100

    NEW: Wall moves and resets when it hits camera pos

---
+++ Super Shapester/Assets/Move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+	public Vector3 velocity;
+	public Rigidbody rb; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = velocity;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {        
+		if (gameObject.transform.position.z <= 0)
+        {
+            gameObject.transform.position = new Vector3(0, 1, 150);
+        }	
+	}
+}
