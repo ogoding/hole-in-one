16e713aa3ec160b26c374e8316483be4e7f0e2df
unknown <7651775@AS409-010.ds.swin.edu.au>
2016-12-03 18:07:54 +1100

    Audio placeholder prefab, added pitch by timescale script.

---
+++ Super Shapester/Assets/Scripts/AudioPitchByTimescale.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioPitchByTimescale : MonoBehaviour 
+{
+	public AudioSource Audio;
+	public float AddPitch;
+	public float AmpPitch;
+
+	void Start ()
+	{
+		if (Audio == null) 
+		{
+			Audio = GetComponent<AudioSource> ();
+		}
+	}
+
+	void Update () 
+	{
+		Audio.pitch = (AmpPitch * Time.timeScale) + AddPitch;
+	}
+}
