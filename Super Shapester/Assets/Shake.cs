5a661cc33391f92f929ba70213337d77bd127258
Matt Noone <matt.noone95@gmail.com>
2016-12-03 21:24:42 +1100

    added screen-shake, bound to R key

---
+++ Super Shapester/Assets/Shake.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shake : MonoBehaviour {
+
+	public Camera camera;
+	public float shake = 0.0f;
+	public float shakeIntensity;
+	public float decreaseIntensityBy;
+
+	// Use this for initialization
+	void Start ()
+	{
+		shakeIntensity = 0.5f;
+		decreaseIntensityBy = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.R))
+			shake = 1.0f;
+		if (shake > 0)
+		{
+			camera.transform.localPosition = Random.insideUnitSphere * shakeIntensity;
+			shake -= Time.deltaTime * decreaseIntensityBy;
+		} 
+		else
+		{
+			shake = 0.0f;
+		}
+	}
+}
