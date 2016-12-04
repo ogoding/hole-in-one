9f48701d324b1096d5b53c1b15d86fdccbb87549
unknown <7651775@AS409-010.ds.swin.edu.au>
2016-12-04 12:33:43 +1100

    Merge fixes

---
+++ Super Shapester/Assets/Scripts/DestroyOrDeactivateOverTime.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOrDeactivateOverTime : MonoBehaviour 
+{
+	public enum type
+	{
+		Destroy,
+		Deactivate
+	}
+
+	public type DestroyType;
+
+	public float timeDuration;
+	public float timeRemaining;
+
+	void Start ()
+	{
+		timeRemaining = timeDuration;
+
+		if (DestroyType == type.Destroy) 
+		{
+			Destroy (gameObject, timeDuration);
+		}
+	}
+
+	void Update ()
+	{
+		if (DestroyType == type.Deactivate) 
+		{
+			timeRemaining -= Time.unscaledDeltaTime;
+			if (timeRemaining < 0) 
+			{
+				GetComponent<DestroyOrDeactivateOverTime> ().enabled = false;
+				gameObject.SetActive (false);
+			}
+		}
+	}
+}
