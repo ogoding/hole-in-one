bef22524d02e6af8f18a2d1986d375ffad786c46
unknown <7651775@AS409-010.ds.swin.edu.au>
2016-12-04 14:04:40 +1100

    Music is like a nightclub yo

---
+++ Super Shapester/Assets/Scripts/BloomIntensityByVolume.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CinematicEffects;
+
+public class BloomIntensityByVolume : MonoBehaviour 
+{
+	public AudioSource Audio;
+	public Bloom bloomScript;
+	public Vector2 Limits;
+	public float offset;
+
+	void Start () 
+	{
+	
+	}
+
+	void Update ()
+	{
+		bloomScript.settings.intensity = Mathf.Clamp(Audio.GetComponent<AudioSourceLoudnessTester>().clipLoudness, Limits.x, Limits.y) + offset;
+	}
+}
